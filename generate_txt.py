6fbfb53fc7860e0fa5c0d81624a0d64f7d5e7cce
rar8393 <114940566+rar8393@users.noreply.github.com>
2022-10-10 13:42:23 -0400

    Add files via upload

--- generate_txt.py
+++ generate_txt.py
@@ -2,36 +2,36 @@ from transformers import GPT2Tokenizer, OPTForCausalLM
 import generated_text_metrics as gtm
 
 class generate_model_results:
-    def __init__(tokenizer, model):
-        self.tokenizer = GPT2Tokenizer.from_pretrained("facebook/opt-350m")
-        self.model = OPTForCausalLM.from_pretrained("facebook/opt-350m")
+    def __init__(self, tokenizer, model):
+        self.tokenizer = tokenizer #GPT2Tokenizer.from_pretrained("facebook/opt-350m")
+        self.model = model #OPTForCausalLM.from_pretrained("facebook/opt-350m")
         
-    def get_embeddings(output):
+    def get_embeddings(self, output):
         '''
         sequence = tokenizer.encode("Here this out. Here", return_tensors="pt")
         embeds = model.get_input_embeddings()(sequence)
         '''
-        return model.get_input_embeddings()(output)
+        return self.model.get_input_embeddings()(output)
         
-    def generate_sample(txt, do_sample=True, sample_size=5, max_length=64, visualize=False):
+    def generate_sample(self, txt, do_sample=True, sample_size=5, max_length=64, visualize=False):
         final_metrics = {}
-        input_ids = tokenizer(txt, return_tensors="pt").input_ids #, return_tensors="pt"
-        outputs = model.generate(input_ids, do_sample=True, num_return_sequences=sample_size, max_new_tokens=max_length)
-        decoded_list = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        input_ids = self.tokenizer(txt, return_tensors="pt").input_ids #, return_tensors="pt"
+        outputs = self.model.generate(input_ids, do_sample=True, num_return_sequences=sample_size, max_new_tokens=max_length)
+        decoded_list = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         final_metrics[txt] = {'metric': gtm.generated_text_metric(txt)}
         for i in range(len(decoded_list)):
             generated_txt = decoded_list[i].replace(txt, '')
             final_metrics[generated_txt] = {'metric': gtm.generated_text_metric(generated_txt), 
                                             'sentence_similarity': gtm.generation_sentence_simialrity(txt, generated_txt),
-                                            'embedding': get_embeddings(outputs[i]) }
+                                            'embedding': self.get_embeddings(outputs[i]) }
             if visualize:
-                gtm.visualize_dependecy_tree(item)
-                gtm.visualize_ne_tree(item)
+                gtm.visualize_dependecy_tree(decoded_list[i])
+                gtm.visualize_ne_tree(decoded_list[i])
         return final_metrics
     
-    def generate_sample_list(txt_list, do_sample=True, sample_size=5, max_length=64, visualize=False):
+    def generate_sample_list(self, txt_list, do_sample=True, sample_size=5, max_length=64, visualize=False):
         result = {}
         for txt in txt_list:
-            result['txt'] = generate_sample(txt, do_sample=do_sample, sample_size=sample_size, 
+            result['txt'] = self.generate_sample(txt, do_sample=do_sample, sample_size=sample_size, 
                                             max_length=max_length, visualize=visualize)
         return result
--- generated_text_metrics.py
+++ generated_text_metrics.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load('en_core_web_sm')
 
